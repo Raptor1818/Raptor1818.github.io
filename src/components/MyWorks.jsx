91a3068373bb764b7b19669ac4f1ede1a555401f
OverKnown1 <francyoppureno@gmail.com>
2023-11-11 11:00:14 +0100

    Finished implementing My Works
    Will add more in the future
    
    - Added icons to show properties of the works,
    - Fixed on mobile
    - Fixed cat image not being the same size as the others

--- src/components/MyWorks.jsx
+++ src/components/MyWorks.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import { FaLightbulb } from "react-icons/fa"
+import React from 'react';
+import { FaLightbulb, FaGithub, FaReact } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
 
 const MyWorks = () => {
 
   const works = [
-    { id: 1, title: 'Raptorino', screenshotUrl: '/img/works/raptorino-com.webp', hrefUrl: 'https://https://github.com/Raptor1818/raptorino'},
-    { id: 2, title: 'Bar Calculator', screenshotUrl: '/img/works/bar-calculator-vercel-app.webp', hrefUrl: 'https://github.com/Raptor1818/bar-calculator'},
-    { id: 3, title: 'WIP...', screenshotUrl: ''}
+    {
+      id: 1,
+      title: 'Raptorino',
+      screenshotUrl: '/img/works/raptorino-com.webp',
+      hrefUrl: 'https://github.com/Raptor1818/raptorino',
+      linkTo: FaGithub,
+      langs: [FaReact, SiTailwindcss]
+    },
+    {
+      id: 2,
+      title: 'Bar Calc',
+      screenshotUrl: '/img/works/bar-calculator-vercel-app.webp',
+      hrefUrl: 'https://github.com/Raptor1818/bar-calculator',
+      linkTo: FaGithub,
+      langs: [FaReact, SiTailwindcss]
+    },
+    {
+      id: 3,
+      title: 'WIP...',
+      screenshotUrl: '/img/works/cat-WIP.webp',
+    }
   ];
 
   return (
@@ -16,16 +35,26 @@ const MyWorks = () => {
             <FaLightbulb className='px-2' size={60} />
             <h1 className="md:text-5xl text-4xl font-semibold mb-2 text-center md:w-auto w-min">My Works</h1>
         </div>
-        <div className='grid md:grid-cols-3 grid-cols-2 gap-8 px-12 md:px-0'>
+        <div className='grid md:grid-cols-3 grid-cols-1 gap-8 px-4 md:px-0'>
           {works.map(work => (
-            <div key={work.id} className='rounded-lg border-[1px] border-gray-900 shadow-md shadow-gray-600'>
-                <a href={work.hrefUrl}>
-                  <img src={work.screenshotUrl} alt={work.title}  className="roudned-md duration-200 hover:scale-105" />
-                </a>
-                <div className='flex items-center justify-center p-4'>
-                  <p>{work.title}</p>
+            <a href={work.hrefUrl} target='_blank' rel="noopener noreferrer" key={work.id}>
+              <div className='rounded-lg border-[1px] border-gray-900 shadow-md shadow-gray-800 duration-200 hover:scale-105 overflow-hidden text-center'>
+                <img 
+                  src={work.screenshotUrl} 
+                  alt={work.title} 
+                  className='w-full h-48 object-cover'
+                />
+                <div className='grid grid-cols-3 justify-center items-center p-4 text-3xl'>
+                  <div className='flex flex-row justify-center items-center gap-2'>
+                    {work.linkTo && <work.linkTo />}
+                  </div>
+                  <p className='text-lg'>{work.title}</p>
+                  <div className='flex flex-row justify-center items-center gap-2'>
+                    {work.langs && work.langs.map((LangIcon, index) => <LangIcon key={index} />)}
+                  </div>
                 </div>
               </div>
+            </a>
           ))}
         </div>
       </div>
@@ -33,4 +62,4 @@ const MyWorks = () => {
   )
 }
 
-export default MyWorks
+export default MyWorks;
